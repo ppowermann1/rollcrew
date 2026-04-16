ce2a67ff5d305de6acf786ef12625006fbadde72
ppowermann1 <tltgogo0@gmail.com>
2026-04-16 16:34:20 +0900

    chore: init project

---
+++ src/main/java/com/rollcrew/rollcrew/RollcrewApplication.java
@@ -0,0 +1,13 @@
+package com.rollcrew.rollcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollcrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollcrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rollcrew/rollcrew/RollcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rollcrew.rollcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollcrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
